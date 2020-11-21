5a7fcd7d7528d9c2c7855bd98f4d168f5dd2e418
Louis JARRIER <louis.jarrier@viacesi.fr>
2020-11-21 14:13:49 +0100

    creation du main (1-6)

--- Test1/Test1/main.cpp
+++ Test1/Test1/main.cpp
@@ -1,10 +1,6 @@
-﻿#include <cstdio>
+﻿#include "include.h"
 
 int main()
 {
-    printf("Bonjour de la part de %s !\n", "Test1");
-    printf("Bonjour de la part de GIThub t'a creer la solution en C imbécile XD !\n", "Test1");
-    printf("Bonjour de la part de c la galère mdr !\n", "Test1");
-    printf("je teste commit")
-    return 0;
+	cout << "main" << endl;
 }
