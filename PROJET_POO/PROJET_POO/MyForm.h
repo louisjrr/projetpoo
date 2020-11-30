ae3182c8f4bfbecb3f028aeb649b68cda87f2c47
emile panchout <ping0yn76@gmail.com>
2020-11-30 08:46:28 +0100

    Fix de la reccup d'ip Fix de la réccup d'ip Modif dans : -CL_CAD.h et .cpp -MyForm.h

--- PROJET_POO/PROJET_POO/MyForm.h
+++ PROJET_POO/PROJET_POO/MyForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "CL_CAD.h"
 
 namespace PROJETPOO {
 
@@ -60,27 +61,30 @@ namespace PROJETPOO {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(77, 16);
+			this->textBox1->Location = System::Drawing::Point(58, 13);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(142, 22);
+			this->textBox1->Size = System::Drawing::Size(108, 20);
 			this->textBox1->TabIndex = 0;
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(37, 19);
+			this->label1->Location = System::Drawing::Point(28, 15);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(20, 17);
+			this->label1->Size = System::Drawing::Size(17, 13);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"IP";
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(245, 11);
+			this->button1->Location = System::Drawing::Point(184, 9);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(95, 32);
+			this->button1->Size = System::Drawing::Size(71, 26);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"connexion";
 			this->button1->UseVisualStyleBackColor = true;
@@ -88,25 +92,37 @@ namespace PROJETPOO {
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(848, 659);
+			this->ClientSize = System::Drawing::Size(636, 535);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
+	
+	/// variables venant des classes 
+	CL_CAD conn1;
+
+	/// 
+	
+
 	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		String^ adresse_ip = Convert::ToString(textBox1->Text);
+		conn1.getIP(textBox1->Text);
+		MessageBox::Show(textBox1->Text);
+	}
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
 };
 }
